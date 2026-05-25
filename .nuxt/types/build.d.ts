b3623153b3855f051e432c973adda653e8c7eda8
Meng Hai <menghaihun39@gmail.com>
2026-05-25 07:53:37 +0700

    3

--- .nuxt/types/build.d.ts
+++ .nuxt/types/build.d.ts
@@ -13,7 +13,7 @@ declare module "#build/root-component.mjs";
 declare module "#build/plugins.server.mjs";
 declare module "#build/test-component-wrapper.mjs";
 declare module "#build/devtools/settings.mjs";
-declare module "#build/runtime.vue-devtools-client.WvQVVAv2zSewk9bVrPKx0m0B_tpHDVaAKN6Pa0H_6XU.js";
+declare module "#build/runtime.vue-devtools-client.ZEluQsMnt9iEf_KxaUk1_DWt7kCeXFLSQf33BZ5AaR0.js";
 declare module "#build/routes.mjs";
 declare module "#build/pages.mjs";
 declare module "#build/router.options.mjs";
